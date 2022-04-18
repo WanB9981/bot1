16ac54edbda82ea1274d578ab56e61c340dae385
WanB9981 <103839605+WanB9981@users.noreply.github.com>
2022-04-18 22:13:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['60164492583'] // Put your number here
+global.mods = ['60164492583'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -17,7 +17,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'apivproject',
+  'https://api.xteam.xyz': 'cristian9407',
   'https://melcanz.com': 'elaina',
   'https://api.lolhuman.xyz': 'pkebgk8248jskrkfm',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
